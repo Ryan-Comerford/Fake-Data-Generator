7ab8fc82fecf6c3d64c17189373ea767e023bb3a
Owner <Owner@MacBook-Pro-5.fios-router.home>
2024-03-21 21:32:12 -0400

    Updating...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MythosMaker",
-    version="0.0.1",
+    version="0.0.2",
     author="Ryan William Comerford",
     author_email="comerford.ryan31@gmail.com",
     description="A simple package to generate fake data",
